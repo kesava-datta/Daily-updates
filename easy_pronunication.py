faf4b93aec0a5c268841334493c1f97fc9a38e27
kesava-datta <polakikesavadatta27@gmail.com>
2025-08-08 10:42:27 +0530

    easy_pronunication.py

---
+++ easy_pronunication.py
@@ -0,0 +1,19 @@
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    s=input()
+    
+    vowels={'a','e','i','o','u'}
+    count = 0
+    hard = False
+    
+    for ch in s:
+        if ch not in vowels:
+            count+=1
+            if count >= 4:
+                hard=True
+                break
+        else:
+            count=0
+
+    print("NO" if hard else "YES")
